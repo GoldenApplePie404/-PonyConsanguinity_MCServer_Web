2c539c121cc4c84fa2c6e74d3919a4d81ffffd31
GoldenApplePie <114842088+GoldenApplePie404@users.noreply.github.com>
2026-02-02 23:43:32 +0800

    Add files via upload

---
+++ data/posts.php
@@ -0,0 +1,36 @@
+<?php
+if (!defined('ACCESS_ALLOWED')) {
+    header('HTTP/1.1 403 Forbidden');
+    exit;
+}
+
+return array (
+  'posts' => 
+  array (
+    0 => 
+    array (
+      'id' => '1769484392',
+      'title' => '官网开发日志',
+      'author' => 'GoldenApplePie',
+      'forum' => 'general',
+      'created_at' => '2026-01-27 03:26:32',
+      'updated_at' => '2026-01-27 03:26:32',
+      'replies' => 0,
+      'views' => 0,
+      'content_file' => '1769484392.md',
+    ),
+    1 => 
+    array (
+      'id' => '1769484319',
+      'title' => 'ReadME',
+      'author' => 'GoldenApplePie',
+      'forum' => 'general',
+      'created_at' => '2026-01-27 03:25:19',
+      'updated_at' => '2026-01-27 03:25:19',
+      'replies' => 0,
+      'views' => 0,
+      'content_file' => '1769484319.md',
+    ),
+  ),
+);
+?>
---
+++ data/sessions.php
@@ -0,0 +1,79 @@
+<?php
+if (!defined('ACCESS_ALLOWED')) {
+    header('HTTP/1.1 403 Forbidden');
+    exit;
+}
+
+return array (
+  'b7c4b4ce-b038-49d9-a15a-3f9f684c133a' => 
+  array (
+    'user_id' => 'c361cebb-3a36-4799-b094-46bc1d81f0f5',
+    'username' => 'GoldenApplePie',
+    'role' => 'admin',
+    'created_at' => '2026-02-01 17:00:43',
+  ),
+  '0046502a-dbc4-47f4-807c-158753224cc6' => 
+  array (
+    'user_id' => '0d01959c-561e-49f1-bc86-48f2c929535f',
+    'username' => 'GoldenApplePiee',
+    'role' => 'user',
+    'created_at' => '2026-02-01 17:01:14',
+  ),
+  'eddb4cfa-eae8-4a6e-b1d8-31627ab82000' => 
+  array (
+    'user_id' => 'c361cebb-3a36-4799-b094-46bc1d81f0f5',
+    'username' => 'GoldenApplePie',
+    'role' => 'admin',
+    'created_at' => '2026-02-01 17:04:00',
+  ),
+  'b7b305f3-de7f-4a88-b7a6-17d5800b0902' => 
+  array (
+    'user_id' => '0d01959c-561e-49f1-bc86-48f2c929535f',
+    'username' => 'GoldenApplePiee',
+    'role' => 'user',
+    'created_at' => '2026-02-01 17:12:11',
+  ),
+  '1883a16e-25da-49cf-a5b1-11f1a8995061' => 
+  array (
+    'user_id' => 'c361cebb-3a36-4799-b094-46bc1d81f0f5',
+    'username' => 'GoldenApplePie',
+    'role' => 'admin',
+    'created_at' => '2026-02-01 17:12:49',
+  ),
+  '3efe83f6-ca84-496f-b41d-344f479d6678' => 
+  array (
+    'user_id' => 'c361cebb-3a36-4799-b094-46bc1d81f0f5',
+    'username' => 'GoldenApplePie',
+    'role' => 'admin',
+    'created_at' => '2026-02-01 17:17:17',
+  ),
+  'ae12f2ad-383e-4f91-8e7b-1e9a5c282c7a' => 
+  array (
+    'user_id' => 'c361cebb-3a36-4799-b094-46bc1d81f0f5',
+    'username' => 'GoldenApplePie',
+    'role' => 'admin',
+    'created_at' => '2026-02-01 17:19:23',
+  ),
+  'c93b8526-e5b5-4c56-a340-e85bbb4e0154' => 
+  array (
+    'user_id' => 'c361cebb-3a36-4799-b094-46bc1d81f0f5',
+    'username' => 'GoldenApplePie',
+    'role' => 'admin',
+    'created_at' => '2026-02-02 03:26:48',
+  ),
+  '5042db37-884d-4431-aa2b-2407cc10efea' => 
+  array (
+    'user_id' => '0d01959c-561e-49f1-bc86-48f2c929535f',
+    'username' => 'GoldenApplePiee',
+    'role' => 'user',
+    'created_at' => '2026-02-02 03:41:32',
+  ),
+  '549cf27d-419e-4995-9bea-9fdc4702b305' => 
+  array (
+    'user_id' => 'c361cebb-3a36-4799-b094-46bc1d81f0f5',
+    'username' => 'GoldenApplePie',
+    'role' => 'admin',
+    'created_at' => '2026-02-02 03:41:50',
+  ),
+);
+?>
